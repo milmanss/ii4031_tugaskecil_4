73a7fdfe889a12ae8722567dd8ecb6b8d4b37fcc
mdhainayya <madiha.ainayya@gmail.com>
2021-04-23 01:27:10 +0700

    add search message

--- src/algoritma_rsa.py
+++ src/algoritma_rsa.py
@@ -129,6 +129,16 @@ def searchSignature(message):
                 signature += message[i]
     return signature
 
+def searchMessage(message):
+    msg = ''
+    start = message.find("<ds>")
+    if "<ds>" in message:
+        end = message.find("</ds>")
+        if "</ds>" in message:
+            for i in range(start):
+                msg += message[i]
+    return msg
+
 # verify jika digital signature di file terpisah TESTED tinggal ulik2 dikit    
 def verify(receivedHash, message, private):
     ourHashed = hashFunction(message) #result hash mentah
@@ -162,6 +172,10 @@ if __name__ == '__main__':
 
     hashed = hashFunction(message)
     sign = encrypt(public, hashed)
+
+    # msg = "hahahahahahahah<ds>nana</ds>"
+    # print(searchMessage(msg))
+    # print("yuhu")
 
     if verify(sign, message, private):
         print("success")
